4f7616575c94de8385ceed917c4de6fd301c9f7e
Sebastian Gurin <sebastigurin@gmail.com>
2018-11-22 04:50:22 -0300

    fixed

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export interface FolderDropManager {
   uninstall(el: HTMLElement, listener: (e: FolderDropManagerEvent) => void)
 }
 export interface FolderDropManagerEvent {
-  type: 'read'|'error'|'finish'
+  type: 'read' | 'error' | 'finish'
   file?: FolderDropManagerFile
   error?: Error
 }
@@ -17,7 +17,7 @@ export interface FolderDropManagerFile {
   isFile: boolean
   entries?: FolderDropManagerFileEntry[]
   isBinary?: boolean
-  content?: string
+  content?: string | ArrayBuffer
   item: FolderDropManagerFileEntry
   file?: FolderDropManagerFileFile
 }
@@ -36,6 +36,10 @@ export interface FolderDropManagerFileEntry {
   name: string
 }
 
+export interface FolderDropManagerConfig {
+  /** if true will read files as array buffer - see property FolderDropManagerFile.content */
+  readAs?: 'string' | 'ByteArray' | 'DataURL'
+}
 /**
  * a FolderDDManager instance can be installed on a DOm Element and mange all the files 
  * (recursively) contained in a dropped folder by the user. Then it will work for collecting 
@@ -43,9 +47,11 @@ export interface FolderDropManagerFileEntry {
  * not extract content of binary files. 
  * @class FolderDDManager
  */
-function  FolderDDManager () { };
-export function createFolderDropManager(): FolderDropManager{
-  return new FolderDDManager()
+function FolderDDManager(config: FolderDropManagerConfig = { readAs: 'DataURL' }) {
+  this.config = config
+};
+export function createFolderDropManager(config: FolderDropManagerConfig = { readAs: 'DataURL' }): FolderDropManager {
+  return new FolderDDManager(config)
 }
 (window as any).createFolderDropManager = createFolderDropManager;
 
@@ -58,103 +64,85 @@ w.resolveLocalFileSystemURL = w.webkitResolveLocalFileSystemURL ||
 
 proto.error = function (e) {
   console.log('error', e);
-  this.notifyListener({type: 'error', error: e})
+  this.notifyListener({ type: 'error', error: e })
   throw e
 };
 proto.error_from_readentries = function (e) {
   console.log('error_from_readentries', e);
-  this.notifyListener({type: 'error', error: e})
+  this.notifyListener({ type: 'error', error: e })
   throw e
 };
-proto.traverseFileTree = function (item, path) {
+proto.traverseFileTree = function (item: any/*{file: File}*/, path) {
   path = path || "";
-  var self = this;
   if (item.isFile) {
     // Get file
-    item.file(function (file) {
-      self.readFileText(file, item, path);
-    }
-      , self.error.bind(self)
-    );
+    (item).file(file =>
+      this.readFileContents(file, item, path)
+
+      , this.error.bind(this)
+    )
   } else if (item.isDirectory) {
 
     // Get folder contents
     var dirReader = item.createReader();
-    dirReader.readEntries(function (entries) {
+    dirReader.readEntries(entries => {
 
       const fileInfo = { item, isFile: false, isDirectory: true, fullPath: item.fullPath, entries }
-      self.notifyListener({ type: 'read', file: fileInfo })
+      this.notifyListener({ type: 'read', file: fileInfo })
 
       if (entries)
         for (var i = 0; i < entries.length; i++) {
-          self.traverseFileTree(entries[i], path + item.name + "/");
+          this.traverseFileTree(entries[i], path + item.name + "/");
         }
     }
-      , self.error_from_readentries.bind(self)
+      , this.error_from_readentries.bind(this)
     );
   }
 };
 /**
-@method isBinary
+@method readFileContents
 */
-proto.isBinary = function (str) {
-  //we guess by comparing form 3rd char to 100 for if charcode>=65533
-  if (str.length < 3) {
-    return false; //the problem are big binary files. 
-  }
-  for (var i = 2; i < Math.min(str.length - 1, 100); i++) {
-    if (str.charCodeAt(i) >= 65533) {
-      return true;
-    }
-  }
-  return false;
-};
-/**
-@method readFileText
-*/
-proto.readFileText = function (file, item, path) {
+proto.readFileContents = function (file, item: any/* WebKitFileEntry*/, path) {
   var reader = new FileReader();
-  var self = this;
-  reader.readAsText(file);
-  reader.addEventListener('loadend', function (e) {
-    var isBinary = self.isBinary(reader.result);
-    const fileInfo = { file, fullPath: item.fullPath, item, isDirectory: item.isDirectory, isBinary, isFile: item.isFile, content: undefined }
-    if (isBinary) {
-      // console.log('BINARYFILE');
-      // TODO: read binary
-    }
-    else {
-      fileInfo.content = reader.result.toString()
-    }
-    self.notifyListener({ type: 'read', file: fileInfo })
+  const handler = (reader) => {
+    const fileInfo = { file, fullPath: item.fullPath, item, isDirectory: item.isDirectory,  isFile: item.isFile, content: reader.result }
+    this.notifyListener({ type: 'read', file: fileInfo })
+  }
+  if (this.config.readAs === 'DataURL') {
+  reader.addEventListener('loadend', (e) => handler(reader))
+    reader.readAsDataURL(file);
+  }
+  else if (this.config.readAs === 'ByteArray') {
 
-  });
+    reader.readAsArrayBuffer(file)
+  }
+  else {
+    reader.readAsText(file);
+  }
 };
 
 // TODO. a very very heuristic method to realize if the data transfer has finished (just a timeout). TODO: research and do this better.
-proto.emitFileEventFinish = function(fn){
-  this.lastFileEventTime = performance.now()
-  this.emitFileEventFinishTimer = setInterval(()=>{
-    if(performance.now()-this.emitFileEventFinishTimer>1000){
+proto.emitFileEventFinish = function (fn) {
+  this.emitFileEventFinishTimer = setInterval(() => {
+    if (performance.now() - this.emitFileEventFinishTimer > 1000) {
       clearInterval(this.emitFileEventFinishTimer)
-      this.notifyListener({type: 'finish'})
+      this.notifyListener({ type: 'finish' })
     }
   }, 1000)
 }
-proto.notifyListener = function(e){
-  this.emitFileEventFinish = performance.now()
+proto.notifyListener = function (e) {
   this.listener(e)
 }
 
-proto.handleDrop = function (evt) {
+proto.handleDrop = function (evt: DragEvent) {
   evt.stopPropagation();
   evt.preventDefault();
-  this.emitFileEventFinish(()=>{
-    this.listener({type: 'finish'})
+  this.emitFileEventFinish(() => {
+    this.listener({ type: 'finish' })
   })
   var items = evt.dataTransfer.items || evt.dataTransfer.files;
   for (var i = 0; i < items.length; i++) {
-    var item = items[i].getAsEntry ? items[i].getAsEntry() : items[i].webkitGetAsEntry ? items[i].webkitGetAsEntry() : undefined;
+    var item = (items[i] as any).getAsEntry ? (items[i] as any).getAsEntry() : (items[i] as DataTransferItem).webkitGetAsEntry ? (items[i] as DataTransferItem).webkitGetAsEntry() : undefined;
     if (item) {
       this.traverseFileTree(item);
     }
@@ -176,12 +164,12 @@ proto.install = function (el: HTMLElement, listener) {
   this.handleDropListenerFn = this.handleDrop.bind(this)
   this.handleDragOverListenerFn = this.handleDragOver.bind(this)
   el.addEventListener("drop", this.handleDropListenerFn, false);
-  el.addEventListener("dragover",this. handleDragOverListenerFn, false);
+  el.addEventListener("dragover", this.handleDragOverListenerFn, false);
 };
 
 
 proto.uninstall = function (el: HTMLElement, listener) {
   delete this.listener
   el.removeEventListener("drop", this.handleDropListenerFn, false);
-  el.addEventListener("dragover",this.handleDragOverListenerFn, false);
+  el.addEventListener("dragover", this.handleDragOverListenerFn, false);
 };
